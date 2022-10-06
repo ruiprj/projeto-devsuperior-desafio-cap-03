b3b770bffb49f9b950e0a5e4da3ccded87753d2f
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-10-06 10:52:07 -0400

    Restricting access to ReviewForm component

--- src/pages/Private/MovieDetails/index.tsx
+++ src/pages/Private/MovieDetails/index.tsx
@@ -4,7 +4,7 @@ import ReviewList from 'components/ReviewList';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Review } from 'types/review';
-import { requestBackend } from 'util/requests';
+import { hasAnyRoles, requestBackend } from 'util/requests';
 
 import './styles.css';
 
@@ -15,36 +15,33 @@ type UrlParams = {
 const MovieDetails = () => {
   const { movieId } = useParams<UrlParams>();
 
-  const [ reviews, setReviews ] = useState<Review[]>([]);
+  const [reviews, setReviews] = useState<Review[]>([]);
 
   useEffect(() => {
     const params: AxiosRequestConfig = {
       url: `/movies/${movieId}/reviews`,
       method: 'GET',
-      withCredentials: true
+      withCredentials: true,
       // params: {
       //   page: 0,
       //   size: 10
       // }
     };
 
-    requestBackend(params)
-      .then((response) => {
-        // console.log("AQUI!");
-        // console.log(response.data);
+    requestBackend(params).then((response) => {
 
-        setReviews(response.data);
-      })
+      setReviews(response.data);
+      
+    });
   }, [movieId]);
 
   return (
     <div className="base-private-container  movie-details-container">
-      <h2>Tela detalhes do filme id: { movieId }</h2>
+      <h2>Tela detalhes do filme id: {movieId}</h2>
 
-      <ReviewForm movieId={ movieId } />
+      {hasAnyRoles(['ROLE_MEMBER']) && <ReviewForm movieId={movieId} />}
 
-      <ReviewList reviews={ reviews } />
-
+      <ReviewList reviews={reviews} />
     </div>
   );
 };
